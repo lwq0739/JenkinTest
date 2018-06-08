5cddff183e7f03712aa1d37d6c7bd1ec2f43e832
lwq <644508810@qq.com>
2018-06-08 10:33:29 +0800

    git tag test

--- app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
+++ app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("wlqlwqlwq");
         System.out.println("wlqlwqlwq");
+        System.out.println("wlqlwqlwq");
         Toast.makeText(this,"dev测试",Toast.LENGTH_SHORT).show();
     }
 }
