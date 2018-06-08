1babc17dfba5ea62e3527ebde9c29e2acb8057f4
lwq <644508810@qq.com>
2018-06-08 09:53:23 +0800

    git test

--- app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
+++ app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("wlqlwqlwq");
+        System.out.println("wlqlwqlwq");
         Toast.makeText(this,"dev测试",Toast.LENGTH_SHORT).show();
     }
 }
