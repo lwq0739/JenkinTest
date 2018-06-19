36c692b3a6706a6169017eeff6175c087a59bc32
lwq <644508810@qq.com>
2018-06-19 12:01:13 +0800

    fetch test2

--- app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
+++ app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("wlqlwqlwq");
-        System.out.println("wlqlwqlwq");
         Toast.makeText(this,"dev测试",Toast.LENGTH_SHORT).show();
     }
 }
