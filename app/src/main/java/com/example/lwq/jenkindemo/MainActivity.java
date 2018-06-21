e9b9e6643f3e4ccbd7e745ca4e69bed096d609ea
lwq <644508810@qq.com>
2018-06-21 13:50:50 +0800

    git  fetch  test3

--- app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
+++ app/src/main/java/com/example/lwq/jenkindemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"dev测试",Toast.LENGTH_SHORT).show();
+        System.out.println("git  fetch  test3");
     }
 }
